995f6f89b2b196b3baec63715b9cf63f342f272b
root <root@drugdealersim.com>
2025-10-04 00:19:26 +0200

    index.js for claude tracking

--- webhook-logger.js
+++ webhook-logger.js
@@ -12,6 +12,17 @@ class WebhookLogger {
     }
 
     setupRoutes() {
+        // CORS middleware - must be first
+        this.app.use((req, res, next) => {
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+            res.header('Access-Control-Allow-Headers', 'Content-Type');
+            if (req.method === 'OPTIONS') {
+                return res.sendStatus(200);
+            }
+            next();
+        });
+
         this.app.post('/log-conversation', (req, res) => {
             try {
                 const { 
